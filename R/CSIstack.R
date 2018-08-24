9c6525ef14ee09c66eee1f168f905bf0428befff
Bryan McCloskey <bmccloskey@usgs.gov>
2018-08-24 12:40:30 -0400

    Dashed lines for interp data; legend updates.

--- R/CSIstack.R
+++ R/CSIstack.R
@@ -38,7 +38,8 @@ CSIstack <- function (csi, dir = paste0(getwd(), "/csi_stacked"), thumbs = F, gr
     st <- which(!is.na(sal[, j + 2]))[1]
     en <- rev(which(!is.na(sal[, j + 2])))[1]
     for (k in st:(en - 11))
-      mwa[k, j] <- mean(sal[k:(k + 11), j + 2], na.rm = T)
+      if (!is.na(sal[k + 11, j + 2]))
+        mwa[k, j] <- mean(sal[k:(k + 11), j + 2], na.rm = T)
   }
   mwa <- rbind(array(NA, c(11, num_sites)), mwa)
   for (j in 1:num_sites) {
@@ -73,15 +74,23 @@ CSIstack <- function (csi, dir = paste0(getwd(), "/csi_stacked"), thumbs = F, gr
       for (k in 1:num_months) rect(as.numeric(xrange[k]), i * int_ht - int_ht, as.numeric(xrange[k + 1]), i * int_ht, col = csi.cols[bin[k]], border = NA)
       for (k in which(!is.na(bin))[1]:rev(which(!is.na(bin)))[1]) if (is.na(bin[k])) rect(as.numeric(xrange[k]), i * int_ht - int_ht, as.numeric(xrange[k + 1]), i * int_ht, col = "gray25", border = NA)
     }
-    if (!is.null(gaps))
+    rm <- mwa_r <- mwa_rx <- NULL
+    if (!is.null(gaps)) {
       for (i in 1:length(gaps)) {
         st <- as.Date(paste0(gaps[i], "-01"))
         en <- seq(st, by = paste(gaplengths[i], "month"), length = 2)[2]
         rect(as.numeric(st), 1 * int_ht - int_ht, as.numeric(en), 1 * int_ht, border = "limegreen", lwd = 3)
+        tmp <- which(xrange == st):(which(xrange == en) - 1)
+        rm <- c(rm, tmp)
+        mwa_r <- c(mwa_r, mwa[(tmp[1] - 1):(rev(tmp)[1] + 1), j], NA)
+        mwa_rx <- c(mwa_rx, (tmp[1] - 1):(rev(tmp)[1] + 2))
       }
+      mwa[rm, j] <- NA
+    }
     abline(h = mean(sal[, j + 2], na.rm = T), lwd = 3, col = "grey28")
     abline(h = quantile(sal[, j + 2], c(.25, .75), na.rm = T), lwd = 3,col = "darkgrey")
     lines(xrange2, mwa[, j], lwd = 3, col = "darkblue")
+    if (!is.null(gaps)) lines(xrange2[mwa_rx], mwa_r, lwd = 3, lty = 3, col = "darkblue")
     axis(1, as.numeric(seq.Date(as.Date(paste0(sal$Year[1], "/1/1")), as.Date(paste0(sal$Year[num_months], "/1/1")), by = "year")), sal$Year[1]:sal$Year[num_months], tck = 0.02, cex.axis = 1.25)
     axis(2, seq(int_ht / 2, max, int_ht), 1:scale, tck = 0.02, las = 1, cex.axis = 1.25)
     rect(as.numeric(xrange[num_months] + 150), -50, as.numeric(xrange[num_months]) + 5000, 5, col = "cyan1", border = NA)
@@ -96,16 +105,16 @@ CSIstack <- function (csi, dir = paste0(getwd(), "/csi_stacked"), thumbs = F, gr
     if (leg) {
       fst <- which(!is.na(sal[, j + 2]))[1]
       lst <- tail(which(!is.na(sal[, j + 2])), 1)
-      leg_txt <- c("", "", "", "", "", "", "", "", "", "12-month rolling salinity average", "Mean", "25th and 75th percentile")
+      leg_txt <- c("", "", "", "", "", "", "", "", "", "Mean", "25th and 75th percentile", "12-month rolling salinity average")
       leg_lty <- c(NA, NA, NA, NA, NA, NA, NA, NA, NA, 1, 1, 1)
       leg_lwd <- c(NA, NA, NA, NA, NA, NA, NA, NA, NA, 3, 3, 3)
-      leg_col <- c(NA, NA, NA, NA, NA, NA, NA, NA, NA, "darkblue", "grey28", "darkgrey")
-      if (!is.null(gaps)) { leg_txt[13] <- "Interpolated data"; leg_lty[13] <- 1; leg_lwd[13] <- 3; leg_col[13] <- "limegreen" }
+      leg_col <- c(NA, NA, NA, NA, NA, NA, NA, NA, NA, "grey28", "darkgrey", "darkblue")
+      if (!is.null(gaps)) { leg_txt[13:14] <- c("Interpolated rolling average", "Interpolated data"); leg_lty[13:14] <- c(3, 1); leg_lwd[13:14] <- 3; leg_col[13:14] <- c("darkblue", "limegreen") }
       tmp <- legend("topleft", leg_txt, lty = leg_lty, lwd = leg_lwd, col = leg_col, inset = c(0.01, 0.01), title = expression(bold("EXPLANATION")))
       text(tmp$rect$left + tmp$rect$w, tmp$text$y[1:2], c("CD, coastal drought; CW, coastal wet", paste0("Period of record: ", sal$Month[fst], "/", sal$Year[fst], " - ", sal$Month[lst], "/", sal$Year[lst])), pos = 2)
       par(usr = c(0, 1, 0, 1))
-      rect(c(0.028, 0.028, 0.028, 0.028, 0.028, 0.053, 0.091, 0.091, 0.091, 0.091, 0.091), c(0.834, 0.796, 0.758, 0.720, 0.682, 0.682, 0.834, 0.796, 0.758, 0.720, 0.682), c(0.048, 0.048, 0.048, 0.048, 0.048, 0.086, 0.111, 0.111, 0.111, 0.111, 0.111), c(0.866, 0.828, 0.790, 0.752, 0.714, 0.714, 0.866, 0.828, 0.790, 0.752, 0.714), lwd = 2, col = c(csi.cols[1:6], rev(csi.cols[7:11])))
-      text(c(0.038, 0.038, 0.038, 0.038, 0.038, 0.070, 0.101, 0.101, 0.101, 0.101, 0.101), c(0.850, 0.812, 0.774, 0.736, 0.698, 0.698, 0.850, 0.812, 0.774, 0.736, 0.698), c("CD4", "CD3", "CD2", "CD1", "CD0", "Normal", "CW4", "CW3", "CW2", "CW1", "CW0"))
+      rect(c(0.028, 0.028, 0.028, 0.028, 0.028, 0.053, 0.053, 0.091, 0.091, 0.091, 0.091, 0.091), c(0.834, 0.796, 0.758, 0.720, 0.682, 0.682, 0.834, 0.834, 0.796, 0.758, 0.720, 0.682), c(0.048, 0.048, 0.048, 0.048, 0.048, 0.086, 0.086, 0.111, 0.111, 0.111, 0.111, 0.111), c(0.866, 0.828, 0.790, 0.752, 0.714, 0.714, 0.866, 0.866, 0.828, 0.790, 0.752, 0.714), lwd = 2, col = c(csi.cols[1:6], "gray25", rev(csi.cols[7:11])))
+      text(c(0.038, 0.038, 0.038, 0.038, 0.038, 0.070, 0.070, 0.101, 0.101, 0.101, 0.101, 0.101), c(0.850, 0.812, 0.774, 0.736, 0.698, 0.698, 0.850, 0.850, 0.812, 0.774, 0.736, 0.698), c("CD4", "CD3", "CD2", "CD1", "CD0", "Normal", "Missing", "CW4", "CW3", "CW2", "CW1", "CW0"), col = c(rep("black", 6), "white", rep("black", 5)))
     }
     dev.off()
   }
